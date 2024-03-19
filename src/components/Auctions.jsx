7b99461d929558ae8d7b462ce8c1997dc393e472
AriamGoitom <117870540+AriamGoitom@users.noreply.github.com>
2024-03-19 13:27:35 +0100

    created new components

---
+++ src/components/Auctions.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Auktioner = () => {
+  const [auktioner, setAuktioner] = useState([]);
+
+  useEffect(() => {
+    const hämtaAuktioner = async () => {
+      const response = await fetch('https://auctioneer.azurewebsites.net/auction/group1');
+      const data = await response.json();
+      setAuktioner(data);
+    };
+
+    hämtaAuktioner();
+  }, []);
+
+  return (
+    <div>
+      <h1>Auctions</h1>
+      <ul>
+        {auktioner.map((auktion) => (
+          <li key={auktion.id}>
+          
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Auktioner;
