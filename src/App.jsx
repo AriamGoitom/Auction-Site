592070881e8cab7586f5b643aa7db90c5fdbec92
88Robert <robert.magnusson1988@gmail.com>
2024-03-21 14:24:08 +0100

    ändrat i App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Auctions from './components/Auctions'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AuctionsDetails from './components/AuctionsDetails';
-import './App.css'
+import React from "react";
+import Auctions from "./components/Auctions";
+import { Route, Routes } from "react-router-dom";
+import AuctionsDetails from "./components/Auctionsdetails";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <>
+      <Auctions />
       <Routes>
-        <Route path="/" exact element={<Auctions />} />
         <Route path="/auktion/:id" element={<AuctionsDetails />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
