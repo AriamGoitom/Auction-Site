364bc57bc235ab678cdfb7a0a482fb3e19eef4e5
ShirinAziz <Shirin.aziz96@outlook.com>
2024-03-25 11:13:13 +0100

    Fixed bug in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
-    <React.StrictMode>
+  <React.StrictMode>
+    <Router>
       <App />
-    </React.StrictMode>
-  </Router>
+    </Router>
+  </React.StrictMode>
 );
