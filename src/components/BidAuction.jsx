f721e868bc83f6b71c4157b6b58f554ab136e132
88Robert <robert.magnusson1988@gmail.com>
2024-03-26 13:35:09 +0100

    addAuction

--- src/components/BidAuction.jsx
+++ src/components/BidAuction.jsx
@@ -1,38 +1,74 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-const BidAuction = ({ auction }) => {
-  const [errorMessage, setErrorMessage] = useState("");
-  const [bidderName, setBidderName] = useState("");
-  const [currentBid, setCurrentBid] = useState(auction.startingBid);
+const BidAuction = (auction) => {
+  const [bid, setBid] = useState({
+    Bidder: "",
+    AuctionID: "",
+    Amount: 500,
+    GroupCode: "l6m",
+  });
 
-  const handleBid = () => {
-    if (!bidderName.trim()) {
-      setErrorMessage("Please eneter your name to place a bid.");
-      return;
-    }
-    setCurrentBid((prevBid) => prevBid + 500);
-    setBidderName("");
-    setErrorMessage("");
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setBid((prevBid) => ({
+      ...prevBid,
+      [name]: value,
+    }));
   };
 
-  const handleNameChange = (e) => {
-    setBidderName(e.target.value);
+  const handleBid = async (e) => {
+    e.preventDefault();
+    if (!bid.Bidder) {
+      alert("Please enter your name before placing a bid.");
+      return;
+    }
+    try {
+      const response = await fetch(
+        "https://auctioneer.azurewebsites.net/bid/l6m/41",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(bid),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to place bid");
+      }
+
+      setBid({
+        Bidder: "",
+        AuctionID: "",
+        Amount: 500,
+        GroupCode: "l6m",
+      });
+
+      const data = await response.json();
+      console.log("Bid placed successfully:", data);
+      alert("Bid placed successfully!");
+    } catch (error) {
+      console.error("Error:", error.message);
+      alert("Failed to place bid");
+    }
   };
 
   return (
     <div>
-      <h3>{auction.title}</h3>
-      <p>Description: {auction.Description}</p>
-      <p>Current Bid: {currentBid}</p>
-      <p>Bidder: {bidderName}</p>
-      <input
-        type="text"
-        placeholder="Enter your name"
-        value={bidderName}
-        onChange={handleNameChange}
-      />
-      <button onClick={handleBid}>Add Bid (+500)</button>
-      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+      <h2>Place Bid</h2>
+      <form onSubmit={handleBid}>
+        <label>Bidder Name:</label>
+        <input
+          type="text"
+          name="Bidder"
+          value={bid.Bidder}
+          onChange={handleChange}
+          required
+        />
+
+        <button type="submit">Place Bid with $500</button>
+      </form>
     </div>
   );
 };
