df03aa8f597c5d786c261a28be479948338ce30f
ShirinAziz <Shirin.aziz96@outlook.com>
2024-03-26 12:12:47 +0100

    added css style

--- src/components/Auctions.jsx
+++ src/components/Auctions.jsx
@@ -3,10 +3,14 @@ import SearchBar from "./SearchBar";
 // import { Link } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 
-const Auctions = () => {                            /* This defines a functional component named Auctions. */
-  const [auctions, setAuctions] = useState([]);     /* a state auctions is created with an empty array as its initial value. setAuctions is a function used to update the value of the auctions state. */
+const Auctions = () => {
+  /* This defines a functional component named Auctions. */
+  const [auctions, setAuctions] = useState(
+    []
+  ); /* a state auctions is created with an empty array as its initial value. setAuctions is a function used to update the value of the auctions state. */
 
-  useEffect(() => {                                 /* Using the useEffect hook to make an API call to fetch auctions. The useEffect hook is used to perform asynchronous operations, in this case, an HTTP request to an API to fetch auctions. When the component is first rendered, the fetchAuctions function will run, and the auctions state will be updated with the data returned from the API. */
+  useEffect(() => {
+    /* Using the useEffect hook to make an API call to fetch auctions. The useEffect hook is used to perform asynchronous operations, in this case, an HTTP request to an API to fetch auctions. When the component is first rendered, the fetchAuctions function will run, and the auctions state will be updated with the data returned from the API. */
     const fetchAuctions = async () => {
       const response = await fetch(
         "https://auctioneer.azurewebsites.net/auction/l6m"
@@ -19,25 +23,28 @@ const Auctions = () => {                            /* This defines a functional
     fetchAuctions();
   }, []);
 
-  return (                                            /* Rendering auctions in the list. Auctions are rendered in a <ul> list with each auction as an <li> element. For each auction, its title, starting price, start date, end date, created by, and bid are displayed. The map method is used to iterate over each auction in the auctions array and generate JSX for each auction. */
+  return (
+    /* Rendering auctions in the list. Auctions are rendered in a <ul> list with each auction as an <li> element. For each auction, its title, starting price, start date, end date, created by, and bid are displayed. The map method is used to iterate over each auction in the auctions array and generate JSX for each auction. */
     <div>
       <div className="h-full flex items-center justify-center">
         <SearchBar icon={<AiOutlineSearch size={25} />} auctions={auctions} />
       </div>
-      <h1>Auctions</h1>
-      <ul>
-      {auctions.map((auction) => (
-          <li key={auction.AuctionID}>
-            <h2>{auction.Title}</h2>
-            <p>Pris: {auction.StartingPrice}</p>
-            <p>Startdatum: {auction.StartDate}</p>
-            <p>Slutdatum: {auction.EndDate}</p>
-            <p>Skapad av: {auction.CreatedBy}</p>
-            <p>Bud: {auction.bud}</p>
-            {/* Lägg till fler egenskaper om det behövs */}
-          </li>
-        ))}
-      </ul>
+      <div className="h-full">
+        <h1 className="font-semibold text-2xl px-8 mt-14">Auctions</h1>
+        <ul className="w-full px-8 flex flex-col mt-10 sm:flex-wrap sm:flex-row item-center gap-14">
+          {auctions.map((auction) => (
+            <li key={auction.AuctionID}>
+              <h2 className="font-semibold">{auction.Title}</h2>
+              <p>Pris: {auction.StartingPrice}</p>
+              <p>Startdatum: {auction.StartDate}</p>
+              <p>Slutdatum: {auction.EndDate}</p>
+              <p>Skapad av: {auction.CreatedBy}</p>
+              <p>Bud: {auction.bud}</p>
+              {/* Lägg till fler egenskaper om det behövs */}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
