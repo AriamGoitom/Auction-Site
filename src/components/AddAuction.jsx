c7d9e130bad83b08cced46a89aaf7868436fb4a5
ShirinAziz <Shirin.aziz96@outlook.com>
2024-03-26 14:30:40 +0100

    added style to Addauction component

--- src/components/AddAuction.jsx
+++ src/components/AddAuction.jsx
@@ -57,64 +57,91 @@ const AddAuction = () => {
 
   return (
     <Link to={"/add"}>
-      <h2>Add New Auction</h2>
-      <form onSubmit={handleSubmit}>
-        <label>Title:</label>
-        <input
-          type="text"
-          name="Title"
-          value={auction.Title}
-          onChange={handleChange}
-          required
-        />
+      <div className="w-full flex flex-col justify-center items-center gap-10 mt-10">
+        <h2 className="text-semibold text-2xl">Add New Auction</h2>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-4 justify-center items-center"
+        >
+          <div className="flex gap-4">
+            <label>Title:</label>
+            <input
+              type="text"
+              name="Title"
+              value={auction.Title}
+              onChange={handleChange}
+              required
+              className="border border-black"
+            />
+          </div>
 
-        <label>Description:</label>
-        <input
-          type="text"
-          name="Description"
-          value={auction.Description}
-          onChange={handleChange}
-          required
-        />
+          <div className="flex gap-4">
+            <label>Description:</label>
+            <input
+              type="text"
+              name="Description"
+              value={auction.Description}
+              onChange={handleChange}
+              required
+              className="border border-black"
+            />
+          </div>
 
-        <label>Start Date:</label>
-        <input
-          type="datetime-local"
-          name="StartDate"
-          value={auction.StartDate}
-          onChange={handleChange}
-          required
-        />
+          <div className="flex gap-4">
+            <label>Start Date:</label>
+            <input
+              type="datetime-local"
+              name="StartDate"
+              value={auction.StartDate}
+              onChange={handleChange}
+              required
+              className="border border-black"
+            />
+          </div>
 
-        <label>End Date:</label>
-        <input
-          type="datetime-local"
-          name="EndDate"
-          value={auction.EndDate}
-          onChange={handleChange}
-          required
-        />
+          <div className="flex gap-4">
+            <label>End Date:</label>
+            <input
+              type="datetime-local"
+              name="EndDate"
+              value={auction.EndDate}
+              onChange={handleChange}
+              required
+              className="border border-black"
+            />
+          </div>
 
-        <label>Starting Price:</label>
-        <input
-          type="number"
-          name="StartingPrice"
-          value={auction.StartingPrice}
-          onChange={handleChange}
-          required
-        />
+          <div className="flex gap-4">
+            <label>Starting Price:</label>
+            <input
+              type="number"
+              name="StartingPrice"
+              value={auction.StartingPrice}
+              onChange={handleChange}
+              required
+              className="border border-black"
+            />
+          </div>
 
-        <label>Created By:</label>
-        <input
-          type="text"
-          name="CreatedBy"
-          value={auction.CreatedBy}
-          onChange={handleChange}
-          required
-        />
-
-        <button type="submit">Add Auction</button>
-      </form>
+          <div className="flex gap-4">
+            <label>Created By:</label>
+            <input
+              type="text"
+              name="CreatedBy"
+              value={auction.CreatedBy}
+              onChange={handleChange}
+              required
+              className="border border-black"
+            />
+          </div>
+          <button
+            type="submit"
+            className="border border-black rounded-full px-4 text-white bg-black mt-6"
+          >
+            Add Auction
+          </button>
+        </form>
+      </div>
     </Link>
   );
 };
